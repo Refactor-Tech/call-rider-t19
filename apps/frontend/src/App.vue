e6dad8d3a8234c56ec102d0232ec039e6d7770eb
matheuspavanelli <mspavanelli@gmail.com>
2025-04-19 14:28:31 -0300

    feat: enhance signup functionality with user feedback and add Playwright tests

--- apps/frontend/src/App.vue
+++ apps/frontend/src/App.vue
@@ -8,6 +8,9 @@ const form = ref({
   password: "",
   is_passenger: false,
 });
+const accountId = ref("");
+const status = ref("");
+const message = ref("");
 
 function fillForm() {
   form.value.name = "John Doe";
@@ -18,32 +21,63 @@ function fillForm() {
 }
 
 async function signup() {
-  await fetch("http://localhost:3000/signup", {
+  const response = await fetch("http://localhost:3000/signup", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify(form.value),
   });
+
+  const output = await response.json();
+  if (output.accountId) {
+    accountId.value = output.accountId;
+    status.value = "User created successfully!";
+  } else {
+    message.value = output.message;
+    status.value = "Error creating user";
+  }
 }
 </script>
 
 <template>
   <div>
-    <input type="text" placeholder="Enter your name" v-model="form.name" />
-    <input type="email" placeholder="Enter your email" v-model="form.email" />
-    <input type="text" placeholder="Enter your CPF" v-model="form.cpf" />
     <input
+      data-testid="form-name"
+      type="text"
+      placeholder="Enter your name"
+      v-model="form.name"
+    />
+    <input
+      data-testid="form-email"
+      type="email"
+      placeholder="Enter your email"
+      v-model="form.email"
+    />
+    <input
+      data-testid="form-cpf"
+      type="text"
+      placeholder="Enter your CPF"
+      v-model="form.cpf"
+    />
+    <input
+      data-testid="form-password"
       type="password"
       placeholder="Enter your password"
       v-model="form.password"
     />
     <label for="is_passenger">
-      <input type="checkbox" v-model="form.is_passenger" />
+      <input
+        data-testid="form-is-passenger"
+        type="checkbox"
+        v-model="form.is_passenger"
+      />
       Passenger
     </label>
-    <button @click="signup">Sign Up</button>
+    <button data-testid="btn-signup" @click="signup">Sign Up</button>
     <button @click="fillForm">Fill</button>
+    <p data-testid="status">{{ status }}</p>
+    <p data-testid="message">{{ message }}</p>
   </div>
 </template>
 
